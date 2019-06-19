622bd19cd503462811f13631fcb4efa301abb696
AlexSmith716 <alexsmith716@outlook.com>
2019-06-18 22:05:34 -0400

    build(bootstrap-loader): Added bootstrap-loader for test of loading styles in webpack and hmr.

--- src/redux/clientMiddleware.js
+++ src/redux/clientMiddleware.js
@@ -122,3 +122,38 @@ export default function clientMiddleware(helpers) {
 //     userId
 //   };
 // }
+
+// export default function clientMiddleware(client) {
+//   // Modeled after redux-thunk
+//   return ({ dispatch, getState }) => next => (action) => {
+//     if (typeof action === 'function') {
+//       return action(dispatch, getState);
+//     }
+// 
+//     // Handle batch actions
+//     if (Array.isArray(action)) {
+//       return action.filter(Boolean).map(dispatch);
+//     }
+// 
+//     const { promise, types, ...rest } = action; // eslint-disable-line no-redeclare
+//     if (!promise) {
+//       return next(action);
+//     }
+// 
+//     const [REQUEST, SUCCESS, FAILURE] = types;
+//     // console.log('types', types);
+//     next({ ...rest, type: REQUEST });
+// 
+//     // Handle a request/promise
+//     const actionPromise = promise(client);
+//     actionPromise.then(
+//       result => next({ ...rest, result, type: SUCCESS }),
+//       error => next({ ...rest, error, type: FAILURE }),
+//     ).catch((error) => {
+//       console.error('MIDDLEWARE ERROR:', error);
+//       next({ ...rest, error, type: FAILURE });
+//     });
+// 
+//     return actionPromise;
+//   };
+// }
